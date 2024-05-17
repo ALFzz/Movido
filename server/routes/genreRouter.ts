9039b064a9633d71d449a05f3266c0d424abf3c4
ALFzz <qiranem@gmail.com>
2024-05-18 02:22:02 +0300

    checkRoleMiddleware created

--- server/routes/genreRouter.ts
+++ server/routes/genreRouter.ts
@@ -1,8 +1,9 @@
 const GenreRouter = require('express');
 const genrerouter = new GenreRouter();
 const genreController = require('../controllers/genreController')
+const checkRole = require('../middleware/checkRoleMiddleware')
 
-genrerouter.post('/', genreController.create)
+genrerouter.post('/', checkRole('ADMIN'), genreController.create)
 genrerouter.get('/', genreController.getAll)
 
 module.exports = genrerouter;
