a152d6df9b62d08478055b597e70e2bb75b8abf6
ALFzz <qiranem@gmail.com>
2024-05-18 02:12:22 +0300

    film add and authorization done

--- server/server.ts
+++ server/server.ts
@@ -8,6 +8,7 @@ const express = require('express')
 const sequelize = require('./db')
 const models = require('./models/models')
 const cors = require('cors')
+const fileUpload = require('express-fileupload')
 const router = require('./routes/server')
 
 
@@ -16,6 +17,8 @@ const PORT = process.env.PORT;
 const app = express();
 app.use(cors());
 app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(fileUpload({}))
 app.use('/api', router)
 
 
