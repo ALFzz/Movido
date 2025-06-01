3386bfe5788b48a02fff9875ecec71487ed82d49
ALFzz <qiranem@gmail.com>
2025-06-01 20:43:07 +0300

    Contacts page adaptivity done

--- src/Pages/Contacts/Contacts.tsx
+++ src/Pages/Contacts/Contacts.tsx
@@ -1,45 +1,24 @@
-import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Header} from '../../Components/Header/Header';
-import './Contacts.scss'
+import { Header } from '../../Components/Header/Header';
+import './Contacts.scss';
 import { Footer } from '../../Components/Footer/Footer';
 
-
-
 export function Contacts() {
-
   return (
-    <>
-     
+      <div className="contacts-page">
+        <Header />
+        <main className="contacts-content">
+          <h1 className='contacts-title'>Контакты</h1>
 
-    <div>
-
-      <h1 className='contacts-title'>Контакты</h1> <br /> <br />
-
-      <h2 className='contacts-subtitle'>ООО «Movido» <br /><br/> 
-      Адрес Местонахождения: 161343, Россия, г. Москва,
-      ул. Примерная, д. 123, офис 456 <br /> <br />
-
-      Телефон: +7 (495) 123-45-67 <br /> <br />
-
-      По вопросам сотрудничества напишите нам на partners@wink.ru 
-      <br /> <br />
-
-      Служба поддержки: Для вопросов и помощи связанных
-      с нашим сервисом, пожалуйста, обращайтесь по 
-      электронной почте на support@movido.com или звоните
-      нам по телефону в рабочее время</h2>
-     
-      
-    </div> 
-
-    <div className='contacts-footer position-absolute  start-0 end-0 '>
-      <Footer/>
-    </div>
-    
-    
-    
- 
-    </>
-  )
+          <h2 className='contacts-subtitle'>
+            ООО «Movido» <br /><br />
+            Адрес: 161343, Россия, г. Москва, ул. Примерная, д. 123, офис 456 <br /><br />
+            Телефон: +7 (495) 123-45-67 <br /><br />
+            По вопросам сотрудничества: partners@wink.ru <br /><br />
+            Служба поддержки: support@movido.com или звоните нам по телефону в рабочее время.
+          </h2>
+        </main>
+        <Footer />
+      </div>
+  );
 }
