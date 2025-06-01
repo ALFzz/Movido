6ebe92bcc78a4e15d15ebf68143588c7140a1c44
ALFzz <qiranem@gmail.com>
2025-06-01 20:45:32 +0300

    FAQ page adaptivity done

--- src/Components/Answer/Answer.tsx
+++ src/Components/Answer/Answer.tsx
@@ -1,22 +1,15 @@
-import './Answer.scss'
+import './Answer.scss';
 
 interface IFaq {
-    question: string,
-    answer: string
+    question: string;
+    answer: string;
 }
 
 export const Answer = (props: IFaq) => {
     return (
-        <>
-            <div className='faq-block '>
-               <h1 className='faq-question'>{props.question}</h1>
-               <h2 className='faq-answer'>{props.answer}</h2>
-            </div>
-
-
-
-        </>
-    )
-
-    
-}
+        <div className='faq-block'>
+            <h1 className='faq-question'>{props.question}</h1>
+            <h2 className='faq-answer'>{props.answer}</h2>
+        </div>
+    );
+};
